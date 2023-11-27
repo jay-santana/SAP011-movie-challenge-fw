a9f53a313904f849fe9ea5304ed04209ed8dcd7b
jay santana <jayannyrayara@gmail.com>
2023-11-27 12:31:07 -0300

    T011: feat - Implementação da requisição da lista de gêneros e  ordenação

--- movie-challenge/src/app/services/tmdb/tmdb.service.ts
+++ movie-challenge/src/app/services/tmdb/tmdb.service.ts
@@ -6,13 +6,63 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class TmdbService {
-  private readonly _URL = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&language=en&api_key=e97b1be635ac3e872cca068f724b47e1&page='
-  
+  private readonly _KEY = 'e97b1be635ac3e872cca068f724b47e1';
+  private readonly _BASE_URL = `https://api.themoviedb.org/3/`;
+  private readonly _APPEND = `?include_adult=false&language=en&api_key=${this._KEY}&page=`;
+  private readonly _SORT = `&sort_by=`;
+  private readonly _DISCOVER = `${this._BASE_URL}discover/movie${this._APPEND}`;
+  private readonly _ID = `${this._BASE_URL}movie`;
+  private readonly _GENRELIST = `${this._BASE_URL}genre/movie/list`;
+  private readonly _SELECTGENRE = `&with_genres=`;
+  private readonly _SEARCH = `&with_keywords=`;
+
   constructor(
     private readonly _HTTP: HttpClient
   ) { }
 
-  getMoviesByPages(page: number): Observable<any> {
-    return this._HTTP.get(`${this._URL}${page}`)
+  getMoviesByPages(page: number, genreId?: string, orderBy?: string, keyWord?: string): Observable<any> {
+    // if (genreId !== 0) {
+    //   return this._HTTP.get(`${this._URL}${page}&with_genres=${genreId}`);
+    // } else {
+    //   return this._HTTP.get(`${this._URL}${page}`);
+    // }
+    console.log(`${this._DISCOVER}${page}${genreId?this._SELECTGENRE+`${genreId}`:``}${orderBy?this._SORT+`${orderBy}`:``}${keyWord?this._SEARCH+`${keyWord}`:``}`)
+    return this._HTTP.get(`${this._DISCOVER}${page}${genreId?this._SELECTGENRE+`${genreId}`:``}${orderBy?this._SORT+`${orderBy}`:``}${keyWord?this._SEARCH+`${keyWord}`:``}`);
   }
+
+  getMoviesById(id: number): Observable<any> {
+    return this._HTTP.get(`${this._ID}/${id}?api_key=${this._KEY}`);
+  }
+
+  getMoviesByListGenre(): Observable<any> {
+    return this._HTTP.get(`${this._GENRELIST}?api_key=${this._KEY}`);
+  }
+
+  getMoviesByGenre(genreId: string): Observable<any> {
+    return this._HTTP.get(`${this._SELECTGENRE}?api_key=${this._KEY}&with_genres=${genreId}`);
+  }
+
+  // getMoviesByOrder(sortBy: any): Observable<any> {
+  //   return this._HTTP.get(`${this._ORDERBY}?api_key=${this._KEY}&sort_by=${sortBy}`);
+  // }
+
+  getMoviesByOrder(sortBy: string): Observable<any> {
+    return this._HTTP.get(`${this._SORT}?api_key=${this._KEY}&sort_by=${sortBy}`);
+  }
+
+  getMoviesBySearch(value: string): Observable<any> {
+    return this._HTTP.get(`${this._SEARCH}?api_key=${this._KEY}&search=${value}`);
+  }
+
+
 }
+
+
+
+// getMoviesByGenre(genreId: any): Observable<any> {
+//   return this._HTTP.get(`${this._SELECTGENRE}?api_key=${this._KEY}&with_genres=${genreId}`);
+// }
+
+// getMoviesByOrder(sortBy: any): Observable<any> {
+//   return this._HTTP.get(`${this._ORDERBY}?api_key=${this._KEY}&sort_by=${sortBy}`);
+// }
